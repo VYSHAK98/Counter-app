c32b6f8ea80083b27c2f653c8b7c9e91340d19cd
vyshak harikumar <vyshakharikumar98@gmail.com>
2023-11-03 22:05:32 +0530

    counterapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+
 function App() {
+
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    setCount(count - 1);
+  };
+
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='counter'>
+      <h1>Counter App</h1>
+      <input className='inputbox' type="number" value={count} readOnly /><br />
+      <button className='buttons' onClick={handleIncrement}>Increment</button>
+      <button className='buttons' onClick={handleDecrement}>Decrement</button>
+      <button className='buttons' onClick={handleReset}>Reset</button>
     </div>
   );
 }
+
 
 export default App;
